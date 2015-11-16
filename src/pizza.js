0ffbc06a0014deb5f35ed5a58376805e2c4b1ea2
Sebastain Misas <juanmisas2@gmail.com>
2015-11-16 17:48:15 -0500

    added iteration 1 to toggle toppings

--- src/pizza.js
+++ src/pizza.js
@@ -1,1 +1,18 @@
 // Write your Pizza Builder JavaScript in this file.
+$(document).on("ready", function () {
+	$(".btn-pepperonni").on("click", function () {
+		$(".pep").fadeToggle(0005);
+	});
+	$(".btn-mushrooms").on("click", function () {
+		$(".btn-mushrooms").toggleClass("active")
+	});
+	$(".btn-green-peppers").on("click", function () {
+		$(".btn-green-peppers").toggleClass("active")
+	});
+	$(".btn-sauce").on("click", function () {
+		$(".btn-sauce").toggleClass("active")
+	});
+	$(".btn-crust").on("click", function () {
+		$(".btn-crust").toggleClass("active");
+	});
+});
